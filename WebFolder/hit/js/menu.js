441858ae55d0f224d978d9b9b98e02f8d3298a52
neha-kapoor <neha.kapoor@tenthmatrix.co.uk>
2016-11-10 18:23:09 +0530

    Applied Scroll to top button

--- WebFolder/hit/js/menu.js
+++ WebFolder/hit/js/menu.js
@@ -117,7 +117,22 @@ $(document).ready(function() {
 				load_navigation_data();
 			}, doneTypingInterval);
 		}
-	});			
+	});		
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});	
 });
 
 function toggle_left_submenu(id){
